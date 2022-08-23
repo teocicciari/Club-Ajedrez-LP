33b0cb4107738e62be861d4a967b00307ea80971
teo cicciari <teocicciari@gmail.com>
2022-08-23 00:59:03 -0300

    cambio a club mode

--- app.js
+++ app.js
@@ -29,15 +29,13 @@ document.onclick = function (e) {
 
 navSlide();
 
-const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
-tl.to(".text", {y: '0%', duration: 1, stagger: 0.25});
-tl.to(".slider", {y: "-100%", duration: 1.5, delay: 0.5});
-tl.to(".intro", {y: "-100%", duration: 1}, "-=1.1");
+const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
 tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1});
 tl.fromTo(".main-heading", {opacity: 0}, {opacity: 1, duration: 1}, "-=1.1");
 tl.fromTo(".btn", {opacity: 0}, {opacity: 1, duration: 1}, "-=1.1");
+
 
 // Nav behavior
 
